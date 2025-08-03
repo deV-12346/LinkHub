097f98a175d882713784206826d4dafece900d56
Dev Raj <dr395108@gmail.com>
2025-08-03 18:47:05 +0530

    updated

--- Front-end/axiosIntance.js
+++ Front-end/axiosIntance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL:"https://your-backend-api-url.com/api",
+  baseURL:"https://link-hub-utie.vercel.app/api",
   withCredentials: true,
 });
 
